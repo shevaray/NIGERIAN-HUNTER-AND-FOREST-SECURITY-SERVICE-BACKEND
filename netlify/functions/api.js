3ddef035da769881e67b303e707ee69219d177fb
Ikemefuna Ezieke <ikemefuna.ezieke@botosoft.net>
2024-03-31 06:35:59 +0100

    update app.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -11,14 +11,15 @@ const usersRouter = require('../../routes/users');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(`${__dirname}/views`));
+app.set('views', path.join("../../views"));
 app.set('view engine', 'jade');
+console.log(__dirname);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(`${__dirname}/public`)));
+app.use(express.static(path.join(`../../public`)));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
